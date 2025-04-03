8ddfdb8bcd773596206a2e42368da50dad57e9c4
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-04 00:07:44 +0100

    Solved 14

---
+++ 14.longest-common-prefix.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=14 lang=typescript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+function longestCommonPrefix(strs: string[]): string {
+    const size = strs.length
+    const first = strs[0]
+    const length = first.length
+    let result = ''
+
+    for (let i = 0; i < length; i++) {
+        const letter = first[i]
+        for (let e = 1; e < size; e++) {
+            if (letter !== strs[e][i]) return result
+        }
+        result += letter
+    }
+
+    return result
+};
+// @lc code=end
+
+console.log(
+    'fl',
+    longestCommonPrefix(["flower","flow","flight"])
+)
