e8430d7462a63cd83733fa7f674916369aa42dae
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-04 04:50:49 +0100

    Added folder for classes

--- 2.add-two-numbers.ts
+++ 2.add-two-numbers.ts
@@ -41,12 +41,3 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     }, null)
 };
 // @lc code=end
-
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
-}
