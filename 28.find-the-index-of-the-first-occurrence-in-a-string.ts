780db5e97c0ad8cd3d0ec140f3f7bade5133cb61
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-08 13:16:01 +0100

    Solved 28

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=28 lang=typescript
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+function strStr(haystack: string, needle: string): number {
+    const hLength = haystack.length
+    const nLength = needle.length
+
+    for (let i = 0; i <= hLength - nLength; i++) {
+        let match = true
+        for (let j = 0; j < nLength; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                match = false
+                break
+            }
+        }
+        if (match) return i
+    }
+
+    return -1
+};
+// @lc code=end
+
+console.log(strStr('sadbutsad', 'sad')) // 0
+console.log(strStr('leetcode', 'leeto')) // 0
