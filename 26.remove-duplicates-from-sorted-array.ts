b2eda168afea7386a627192abc8f31754252079a
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-08 12:52:39 +0100

    Removed comments from 26

--- 26.remove-duplicates-from-sorted-array.ts
+++ 26.remove-duplicates-from-sorted-array.ts
@@ -23,10 +23,3 @@ function removeDuplicates(nums: number[]): number {
     return uniques
 };
 // @lc code=end
-
-// console.log(removeDuplicates([1,1,2])) //2
-// console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) // 5
-// console.log(removeDuplicates([1,1])) // 1
-// console.log(removeDuplicates([1,1,1])) // 1
-// console.log(removeDuplicates([1,1,2])) // 2
-console.log(removeDuplicates([1,2])) // 2
