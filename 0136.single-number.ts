fb0417ff64bab57d962194e0244072c1dce6dc37
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-09 22:31:11 +0100

    Perfected 136

---
+++ 0136.single-number.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=136 lang=typescript
+ *
+ * [136] Single Number
+ */
+
+// @lc code=start
+function singleNumber(nums: number[]): number {
+    let result = 0
+
+    for (let i = nums.length; i >= 0; i--) {
+        result ^= nums[i]
+    }
+
+    return result
+};
+// @lc code=end
+
+console.log(singleNumber([4,1,2,1,2]))
