573fafef3e1cc8e721cc9b651ceb539e24064788
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-22 17:45:17 +0100

    Removed console logs

--- 0326.power-of-three.ts
+++ 0326.power-of-three.ts
@@ -10,7 +10,3 @@ function isPowerOfThree(n: number): boolean {
     return Math.log10(n) / 0.47712125471966244 % 1 === 0
 };
 // @lc code=end
-
-console.log(isPowerOfThree(27))
-console.log(isPowerOfThree(0))
-console.log(Math.log10(3))
