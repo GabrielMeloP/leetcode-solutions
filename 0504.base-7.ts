2ffd95e4f1b5af8c047b197e62bf2114c3c4353e
Gabriel Melo <gabriel.melo.p@gmail.com>
2025-04-24 18:57:10 +0100

    Perfected 504

---
+++ 0504.base-7.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=504 lang=typescript
+ *
+ * [504] Base 7
+ */
+
+// @lc code=start
+function convertToBase7(num: number): string {
+    return num.toString(7)
+};
+// @lc code=end
